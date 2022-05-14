61662eb1ab9dbf5e4776ebec8d15671395dece1f
Florian Zwoch <fzwoch@gmail.com>
2022-05-14 10:52:23 +0200

    source: accept some more color formats (10/12 bits)

--- gstreamer-source.c
+++ gstreamer-source.c
@@ -193,6 +193,18 @@ static GstFlowReturn video_new_sample(GstAppSink *appsink, gpointer user_data)
 	case GST_VIDEO_FORMAT_YVYU:
 		frame.format = VIDEO_FORMAT_YVYU;
 		break;
+	case GST_VIDEO_FORMAT_I420_10LE:
+		frame.format = VIDEO_FORMAT_I010;
+		break;
+	case GST_VIDEO_FORMAT_P010_10LE:
+		frame.format = VIDEO_FORMAT_P010;
+		break;
+	case GST_VIDEO_FORMAT_I422_10LE:
+		frame.format = VIDEO_FORMAT_I210;
+		break;
+	case GST_VIDEO_FORMAT_Y444_12LE:
+		frame.format = VIDEO_FORMAT_I412;
+		break;
 	default:
 		frame.format = VIDEO_FORMAT_NONE;
 		blog(LOG_ERROR, "Unknown video format: %s",
@@ -315,7 +327,7 @@ static void create_pipeline(data_t *data)
 	GError *err = NULL;
 
 	gchar *pipeline = g_strdup_printf(
-		"videoconvert name=video ! video/x-raw, format={I420,NV12,BGRA,BGRx,RGBx,RGBA,YUY2,YVYU,UYVY} ! appsink name=video_appsink "
+		"videoconvert name=video ! video/x-raw, format={I420,NV12,BGRA,BGRx,RGBx,RGBA,YUY2,YVYU,UYVY,I420_10LE,P010_10LE,I420_12LE,Y444_12LE} ! appsink name=video_appsink "
 		"audioconvert name=audio ! audioresample ! audio/x-raw, format={U8,S16LE,S32LE,F32LE}, channels={1,2,3,4,5,6,8}, layout=interleaved ! appsink name=audio_appsink "
 		"%s",
 		obs_data_get_string(data->settings, "pipeline"));
